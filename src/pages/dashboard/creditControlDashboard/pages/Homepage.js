e9cf8bd34032736668451651d961992d1176e70e
AnishMandal939 <mandalktaanees12@gmail.com>
2024-09-29 16:23:45 +0545

    feat - api consumed and fetched data in ui

--- src/pages/dashboard/creditControlDashboard/pages/Homepage.js
+++ src/pages/dashboard/creditControlDashboard/pages/Homepage.js
@@ -1,13 +1,29 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import { useMemo } from "react";
 import { Button, Container } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useQuery } from "react-query";
+import { useDispatch, useSelector } from "react-redux";
 import { setAppToken } from "redux/action/token/tokenAction";
 
 export default function Homepage() {
+  const { isAuthenticated } = useAuth0();
   const { logout } = useAuth0();
   const dispatch = useDispatch();
+  const token = useSelector((state) => state?.appToken?.token);
+  const config = useMemo(() => {
+    if (!!token) {
+      return {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+    }
+    return null;
+  }, [token]);
 
   const handleLogout = () => {
     dispatch(setAppToken(""));
@@ -18,6 +34,25 @@ export default function Homepage() {
       federated: true,
     });
   };
+
+  const { data: twotwentyKData } = useQuery({
+    queryKey: ["twotwentyKData"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BASE_URL}category/nft_card_category/param?limit=10&offset=0`,
+          config
+        );
+        return response?.data;
+      } catch (error) {
+        console.log(error);
+        throw error; // Ensure the error is propagated
+      }
+    },
+    enabled: isAuthenticated,
+    refetchOnWindowFocus: false,
+  });
+
   return (
     <>
       <div className="mx-4 py-3">
@@ -32,7 +67,29 @@ export default function Homepage() {
             </Button>
           </div>
           <div>
-          <h2>Welcome to dashboard</h2>
+            <h2>Welcome to dashboard</h2>
+            {twotwentyKData && (
+              <>
+                <div className="row">
+                  {twotwentyKData?.data?.map((data, index) => (
+                    <div className="col-4 py-1" key={index}>
+                      <div className="card">
+                        <div className="card-header">{data?.name}</div>
+                        <div className="card-body">
+                          <ul>
+                            {data?.properties?.map((property, index) => (
+                              <li key={index}>
+                                {property?.title}: {property?.text}
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
           </div>
         </Container>
       </div>
